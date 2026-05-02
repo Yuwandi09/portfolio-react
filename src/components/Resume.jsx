2528bdf787796ffa7afd77a9a113639073cd58ab
Yuwandi09 <yuwandisandanayake@gmail.com>
2026-05-02 10:18:24 +0530

    resume changes

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,66 +8,59 @@ function Resume() {
           <p>Enthusiastic Software Engineering undergraduate seeking internship opportunities.</p>
         </div>
 
-        <div className="row">
-          <div className="col-lg-6">
-
-            <h3 className="resume-title">Summary</h3>
-            <div className="resume-item pb-0">
+       <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">Summary</h3>
+            <div class="resume-item pb-0">
               <h4>Yuwandi Sandanayake</h4>
-              <p>I'm an enthusiastic and motivated individual with a passion for programming and problem-solving. Currently seeking internship opportunities.</p>
+              <p><em>
+                I'm an enthusiastic and motivated individual with a passion for programming and problem-solving. I enjoy learning new technologies and building practical projects. I'm currently seeking internship opportunities where I can gain hands-on experience and contribute to real-world solutions.
+              </em></p>
               <ul>
                 <li>Sri Lanka</li>
-                <li>yuwandisandanayake@gmail.com</li>
+                <li><a href="mailto:yuwandisandanayake@gmail.com">yuwandisandanayake@gmail.com</a></li>
                 <li>+94 743 537 714</li>
               </ul>
             </div>
 
-            <h3 className="resume-title">Experience</h3>
-            <div className="resume-item">
+            <h3 class="resume-title">Experience</h3>
+            <div class="resume-item">
               <h4>No Work Experience Yet</h4>
-              <p>Currently looking for internship opportunities to gain hands-on industry experience.</p>
+              <p><em>Currently looking for internship opportunities to gain hands-on industry experience.</em></p>
             </div>
-
-            <div className="text-center mt-4">
-              <a href="/Yuwandi-CV.pdf" download="Yuwandi-Sandanayake-CV.pdf">
-                <button className="show-all">Download CV</button>
-              </a>
-            </div>
-
           </div>
 
-          <div className="col-lg-6">
-
-            <h3 className="resume-title">Education</h3>
-            <div className="resume-item">
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
               <h4>B.Sc. Hons in Software Engineering</h4>
-              <h5>2025 - Present</h5>
-              <p>University of Kelaniya, Sri Lanka</p>
-              <p>Currently pursuing an honours degree in Software Engineering.</p>
+              <h5>2025 – Present</h5>
+              <p><em><a rel="noopener" href="https://www.kln.ac.lk/" target="_blank">University of Kelaniya, Sri Lanka</a></em></p>
+              <p>Currently pursuing an honours degree in Software Engineering, focusing on programming, algorithms, software design and modern development practices.</p>
             </div>
 
-            <h3 className="resume-title">Projects</h3>
-            <div className="resume-item">
+            <h3 class="resume-title">Projects</h3>
+            <div class="resume-item">
               <h4>Amazon Clone</h4>
-              <p>HTML & CSS</p>
-              <p>Recreated the Amazon homepage layout to practise responsive web design.</p>
+              <p><em>HTML &amp; CSS – <a href="https://github.com/Yuwandi09/amazon-clone" target="_blank" rel="noopener">GitHub</a></em></p>
+              <p>Recreated the Amazon homepage layout to practise responsive web design and CSS structuring.</p>
             </div>
-
-            <div className="resume-item">
+            <div class="resume-item">
               <h4>YouTube Clone</h4>
-              <p>HTML & CSS</p>
-              <p>Built a YouTube frontend UI clone to strengthen CSS skills.</p>
+              <p><em>HTML &amp; CSS – <a href="https://github.com/Yuwandi09/Youtube-clone" target="_blank" rel="noopener">GitHub</a></em></p>
+              <p>Built a YouTube frontend UI clone to strengthen my skills in layout design and CSS styling.</p>
             </div>
-
-            <div className="resume-item">
+            <div class="resume-item">
               <h4>2D Game</h4>
-              <p>Developed a 2D game project to explore game logic.</p>
+              <p><em><a href="https://github.com/Yuwandi09/2D-Game" target="_blank" rel="noopener">GitHub</a></em></p>
+              <p>Developed a 2D game project to explore game logic and apply programming fundamentals.</p>
             </div>
-
           </div>
         </div>
 
       </div>
+
+      
     </section>
   )
 }
